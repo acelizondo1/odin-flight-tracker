6ce4edab13f1331de9d8b1c38a1db55fbdee3a62
Aaron Elizondo <acelizondo11@gmail.com>
2021-09-22 16:44:55 -0500

    Creating airport db data with seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+airports = Airport.create([
+    {city: 'Detroit', state: 'MI', airport_code: 'DTW'},
+    {city: 'Minneapolis', state: 'MN', airport_code: 'MSP'},
+    {city: 'Chicago', state: 'IL', airport_code: 'ORD'},
+    {city: 'San Francisco', state: 'CA', airport_code: 'SFO'},
+    {city: 'Los Angeles', state: 'CA', airport_code: 'LAX'},
+    {city: 'New York', state: 'NY', airport_code: 'JFK'},
+    {city: 'Miami', state: 'FL', airport_code: 'MIA'},
+    {city: 'Dallas', state: 'TX', airport_code: 'DFW'},
+    {city: 'Denver', state: 'CO', airport_code: 'DEN'}
+])
