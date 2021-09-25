dd3532432ce2753fb20b2bce909e826d341499fa
Aaron Elizondo <acelizondo11@gmail.com>
2021-09-25 09:40:26 -0500

    finishing initial search form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,7 +3,13 @@ class FlightsController < ApplicationController
     def index
         @from_airport = Airport.all.map { |a| ["#{a.city}, #{a.state} (#{a.airport_code})", a.id] }
         @to_airport = Airport.all.map { |a| ["#{a.city}, #{a.state} (#{a.airport_code})", a.id] }
-        @departure_times = Flight.all.map { |f| [f.departure_time.strftime("%l:%M %p %m-%d-%Y"), f.id] }
+        @departure_times = generate_dates.map { |d| [d.strftime("%m-%d-%Y"), d] }
 
+    end
+
+
+    private
+    def generate_dates
+        dates = Flight.distinct.order("date(departure_time)").pluck("date(departure_time)")
     end
 end
