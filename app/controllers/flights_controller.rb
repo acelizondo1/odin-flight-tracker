232dbda2d1cb2075bf065c495ccf68c1bf3e23aa
Aaron Elizondo <acelizondo11@gmail.com>
2021-09-24 23:08:19 -0500

    Adding fields to flight search

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,8 +1,9 @@
 class FlightsController < ApplicationController
 
     def index
-        @from_airport = Airport.all.map{ |a| ["#{a.city}, #{a.state} (#{a.airport_code})", a.id] }
+        @from_airport = Airport.all.map { |a| ["#{a.city}, #{a.state} (#{a.airport_code})", a.id] }
         @to_airport = Airport.all.map { |a| ["#{a.city}, #{a.state} (#{a.airport_code})", a.id] }
+        @departure_times = Flight.all.map { |f| [f.departure_time.strftime("%l:%M %p %m-%d-%Y"), f.id] }
 
     end
 end
